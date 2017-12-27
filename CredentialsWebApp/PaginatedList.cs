b7a06986f9a1fb91c385a9dab97468d4288159f2
samohung390 <samohung390@gmail.com>
2017-12-27 17:55:34 +0800

    b

--- CredentialsWebApp/PaginatedList.cs
+++ CredentialsWebApp/PaginatedList.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CredentialsWebApp
 {
-
+    
         public class PaginatedList<T> : List<T>
         {
             public int PageIndex { get; private set; }
